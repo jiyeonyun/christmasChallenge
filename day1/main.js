ad9fb33f253eb402b823cd5795c3ab7a08b28ce0
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-11 00:52:46 +0900

    🎅🏻stretch goals add

--- day1/main.js
+++ day1/main.js
@@ -1,4 +1,5 @@
-const countdownDisplay = document.getElementById("countdown-display")
+const countdownDisplay = document.getElementById("countdown-display");
+const countdownDisplay_ny = document.getElementById("countdown-display_ny");
 
 function renderCountdown(){
     const christmas = 25
@@ -7,16 +8,27 @@ function renderCountdown(){
     // - Calculate remaining days.
     // - Display remaining days in countdownDisplay.
     var today = new Date();
-    console.log(today);
     const today_date = today.getDate();
-     console.log(today_date);
     let remaining_day = christmas - today_date;
-    console.log(remaining_day);
     countdownDisplay.innerText = remaining_day;
 }
+ function newyear(){
+     const setDate =new Date("2022-01-01T00:00:00+0900");
+     const today = new Date();
+     const distance =setDate.getTime() - today.getTime();
 
-renderCountdown()
-
+    const day = Math.floor(distance/(1000*60*60*24));//밀리초라 1000 60초 60분 24시간
+    const hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+    const minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+    const seconds = Math.floor((distance % (1000*60))/(1000));
+    countdownDisplay_ny.innerText = `${day}Days ${hours}hours ${minutes}minutes ${seconds}seconds 🎅🏻`;
+    }
+ 
+ 
+ 
+renderCountdown();
+newyear();
+setInterval(newyear,1000);
 // Stretch goals:
 // - Display hours, minutes, seconds.
 // - Add a countdown for another festival, your birthday, or Christmas 2022.
