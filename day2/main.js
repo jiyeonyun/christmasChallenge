f17f4d03a8e364d11d18610d2fed81b757afb566
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-11 01:36:17 +0900

    ⛄️ add more theme , toggle

--- day2/main.js
+++ day2/main.js
@@ -2,6 +2,7 @@ const body = document.getElementById("body")
 const greeting = document.getElementById("greeting")
 const christmasBtn = document.getElementById("christmas")
 const snowBtn = document.getElementById("snow")
+const special =document.querySelector(".hallo");
 
 // Task:
 //- Add the functionality to switch the theme between 'Christmas' and 'snow'.
@@ -17,3 +18,23 @@ christmasBtn.addEventListener('click',()=>{
 // - Add more themes!
 // - Allow the user to customise the themes.
 // - Turn the radio buttons into a toggle switch.
+var i = 1;
+special.addEventListener('click',()=>{
+   togglehallo();
+});
+
+
+
+function togglehallo(){
+   if(i === 1){
+      body.setAttribute('class','halloween'); 
+      greeting.innerText = `boo~~~!!!!!👻`;
+      i++
+   }
+   else{
+      body.setAttribute('class','christmas'); 
+      greeting.innerText = `🎅 Merry Christmas!`;
+      i--;
+   }
+}
+console.log(i);
