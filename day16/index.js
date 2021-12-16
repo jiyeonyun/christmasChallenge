1eb2d72a7aab50a016c7eab74b06f16b758505ad
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-16 22:07:08 +0900

    🎅🏻christmas movieSelector

---
+++ day16/index.js
@@ -0,0 +1,40 @@
+const ageSelector = document.getElementById("age-selector")
+const genreSelector = document.getElementById("genre-selector")
+
+const btn = document.getElementById("btn")
+const suggestedMovie = document.getElementById("suggested-movie")
+
+const moviesArr = [
+    {
+        name: "Die Hard",
+        age: "18+",
+        genre: "Action"
+    },
+    {
+        name: "Love Actually",
+        age: "18+",
+        genre: "Romance"
+    },
+    {
+        name: "The Polar Express",
+        age: "PG",
+        genre: "Action"
+    },
+    {
+        name: "Shrek",
+        age: "PG",
+        genre: "Romance"
+    }
+]
+
+btn.addEventListener('click',()=>{
+    for(let i = 0 ; i<moviesArr.length; i++){
+         if(ageSelector.value == moviesArr[i].age && genreSelector.value == moviesArr[i].genre){
+        suggestedMovie.innerText = moviesArr[i].name;
+    }
+    }
+   
+});
+// Task: 
+// - Write a function to select a suitable movie based on the age group and genre provided.
+//  - Display it in the suggested-movie paragraph when the button is clicked.
