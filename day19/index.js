3555d2e92ed7ac72a745b5cf8a3ac47cba95e8f3
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-20 21:37:35 +0900

    color

--- day19/index.js
+++ day19/index.js
@@ -1,22 +1,16 @@
 const btn = document.getElementById("btn")
-const foodHolder = document.getElementById('foodHolder');
-var datas = '';
-fetch('https://foodish-api.herokuapp.com/api/images/dessert')
-    .then((response) => response.json())
-    .then(json => {datas = json}
-    );
-        
-
 btn.addEventListener("click", findYum)
-function findYum(){
-    for(let i = 0; i<3; i++){
-        let img = document.createElement('img');
-          img.setAttribute('src' , `${datas.image}`);
-          foodHolder.appendChild(img);
-    }
-   
+
+async function findYum() {
+    const response = await fetch('https://foodish-api.herokuapp.com/api/images/dessert');
+    const yum = await response.json();
     
+    const image1 = yum.image;
+    const foodHolder = document.getElementById("foodHolder")
+    foodHolder.innerHTML = `<img src=${image1}>`;
+
 }
+
 /* Task:
 Call the Foodish API (https://foodish-api.herokuapp.com/) and display random images of desserts on the click of a button.
 
---
+++ day20/index.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll(".controls input");
+const eye = document.querySelector('.eye');
+const nose = document.querySelector('.nose');
+const button = document.querySelector('.button');
+const color = document.querySelector('#color');
+const color2 = document.querySelector('#color2');
+const color3 = document.querySelector('#color3');
+
+// Task:
+// Write a function to update the snowman colors
+// according to the colors selected from the pickers.
+
+
+
+// Stretch goals:
+// - Add other items eg scarf, arms, etc.
+// - Add different options for nose shape, or hats.
+// - Check for contrast between pupils and eye color.
